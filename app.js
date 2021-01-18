971b08d9269573bc647622c95b06cd0b163ebdcd
jagadishpatil <jagadishpatil231@gmail.com>
2021-01-19 00:48:59 +0530

    done

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var txtInput = document.querySelector("#txt-input");
 
 var outputDiv = document.querySelector("#output");
 
-var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
 function getTranslationURL(text){
     return serverURL + "?" + "text=" +text;
@@ -15,16 +15,20 @@ function errorHandler(){
     alert("something wrong with server! try again after some time");
 }
 
-btnTranslate.addEventListener("click", function clickEventHandler(){
-    //outputDiv.innerText = "welcome " + txtInput.value;
+function clickHandler(){
 
-    var inputText = txtInput.nodeValue; //taking input
+    var inputText = txtInput.value;     //taking input
 
     //calling server for processing
     fetch(getTranslationURL(inputText))
     .then(response => response.json())
-    .then(json => console.log(json.contents.translated)
-    .catch(errorHandler))
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;   //output
+    })
+    .catch(errorHandler);
 
-});
+};
+
+btnTranslate.addEventListener("click", clickHandler);
 
