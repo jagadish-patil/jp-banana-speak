66f63964861c66b110a51ac158bf4dd44c06bb07
jagadishpatil <jagadishpatil231@gmail.com>
2021-01-19 00:35:06 +0530

    worked with mock URL

--- app.js
+++ app.js
@@ -4,9 +4,27 @@ var txtInput = document.querySelector("#txt-input");
 
 var outputDiv = document.querySelector("#output");
 
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+function getTranslationURL(text){
+    return serverURL + "?" + "text=" +text;
+}
+
+function errorHandler(){
+    console.log("error occured ", error);
+    alert("something wrong with server! try again after some time");
+}
+
 btnTranslate.addEventListener("click", function clickEventHandler(){
-    console.log("clicked!");
-    console.log("input ", txtInput.value);
-    outputDiv.innerText = "welcome " + txtInput.value;
+    //outputDiv.innerText = "welcome " + txtInput.value;
+
+    var inputText = txtInput.nodeValue; //taking input
+
+    //calling server for processing
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => console.log(json.contents.translated)
+    .catch(errorHandler))
+
 });
 
