68721c378cefe46473a12360c621e5f52a626fee
jagadishpatil <jagadishpatil231@gmail.com>
2021-01-19 00:10:12 +0530

    input output processing done

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ var btnTranslate = document.querySelector("#btn-translate");
 
 var txtInput = document.querySelector("#txt-input");
 
+var outputDiv = document.querySelector("#output");
+
 btnTranslate.addEventListener("click", function clickEventHandler(){
     console.log("clicked!");
     console.log("input ", txtInput.value);
+    outputDiv.innerText = "welcome " + txtInput.value;
 });
 
